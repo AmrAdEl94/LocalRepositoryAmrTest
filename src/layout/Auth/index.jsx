8dde3021840cbc8ae427a2645cc8136d83a0b749
AmrAdEl94 <amr2010adel@gmail.com>
2026-05-06 19:32:23 +0300

    just do

--- src/layout/Auth/index.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-// ==============================|| AUTH LAYOUT ||============================== //
-
-export default function AuthLayout({ children }) {
-  return children;
-}
-
-AuthLayout.propTypes = { children: PropTypes.node };
