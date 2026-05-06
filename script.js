9e72ff8ac0daf9ab87579cb2a0de4f7652226b32
AmrAdEl94 <amr2010adel@gmail.com>
2026-05-06 19:50:13 +0300

    do

--- script.js
+++
@@ -1,8 +0,0 @@
-const sidebar = document.getElementById('sidebar');
-const toggleSidebar = document.getElementById('toggleSidebar');
-
-if (sidebar && toggleSidebar) {
-  toggleSidebar.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-  });
-}
